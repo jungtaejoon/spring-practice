ab42088835d7dfe540dc469dd5d0fb55482751a7
jungtaejoon <jungtaejoon@naver.com>
2017-06-01 03:23:02 +0900

    2

--- spring-practice/src/main/java/kr/or/connect/BookLauncher.java
+++ spring-practice/src/main/java/kr/or/connect/BookLauncher.java
@@ -3,7 +3,7 @@ package kr.or.connect;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import kr.or.connect.config.AppConfig;
+import kr.or.connect.domain.Book;
 import kr.or.connect.persistence.BookDao;
 
 public class BookLauncher {
@@ -12,8 +12,10 @@ public class BookLauncher {
 		// TODO Auto-generated method stub
 		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		BookDao dao = context.getBean(BookDao.class);
+		Book book = new Book("네이버 Java", "김강산", 512);
 		int count = dao.countBooks();
 		System.out.println(count);
+		System.out.println(dao.selectById(dao.insert(book)));
 		context.close();
 	}
 
--- spring-practice/src/main/java/kr/or/connect/persistence/BookDao.java
+++ spring-practice/src/main/java/kr/or/connect/persistence/BookDao.java
@@ -1,23 +1,47 @@
 package kr.or.connect.persistence;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import kr.or.connect.domain.Book;
 
 @Repository
 public class BookDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private static final String COUNT_BOOK = "SELECT COUNT(*) FROM book";
+	private static final String SELECT_BY_ID = "SELECT id, title, author, pages FROM book where id = :id";
+	private RowMapper<Book> rowMapper = BeanPropertyRowMapper.newInstance(Book.class);
+	private SimpleJdbcInsert insertAction;
 	
 	public BookDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+		this.insertAction = new SimpleJdbcInsert(dataSource)
+				.withTableName("book")
+				.usingGeneratedKeyColumns("id");
 	}
 	public int countBooks() {
 		Map<String, Object> params = Collections.emptyMap();
 		return jdbc.queryForObject(COUNT_BOOK, params, Integer.class);
+	}
+	public Book selectById(Integer id) {
+
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", id);
+		return jdbc.queryForObject(SELECT_BY_ID, params, rowMapper);
+	} 
+	public Integer insert(Book book) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(book);
+		return insertAction.executeAndReturnKey(params).intValue();
 	}
 }
